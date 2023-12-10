a25d519e393994811f1759033aefe07cee5396b9
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-12-10 12:50:09 +0200

    ts 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,29 @@
+class Key {
+    private signature: number
+    constructor() {
+ this.signature = Math.random();
+    }
+  
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+}
+
+abstract class House {
+    door = false;
+    key,
+    comeIn(person: Person) {
+        
+    }
+    abstract openDoor(key: Key): boolean
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +32,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
